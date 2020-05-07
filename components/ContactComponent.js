d79b74bd0fb10499cdeee628f8e353587fb4fe47
Joe Peterson <josephbpeterson@gmail.com>
2020-05-06 22:42:18 -0700

    Gestures Part 1

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { ScrollView, Text, View } from "react-native";
 import {Card} from "react-native-elements";
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
 
@@ -11,15 +12,17 @@ class Contact extends Component {
     render() {
         return(
             <ScrollView>
-                <Card title="Conact Information" wrapperStyle={{margin: 20}}>
-                    <View>
-                        <Text> 1 Nucamp Way</Text>
-                        <Text > Seattle, WA 98001</Text>
-                        <Text style={{marginBottom: 10}}> U.S.A. </Text>
-                        <Text> Phone: 1-206-555-1234</Text>
-                        <Text> campsites@nucamp.co</Text>
-                    </View>
-                </Card>
+                <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+                    <Card title="Conact Information" wrapperStyle={{margin: 20}}>
+                        <View>
+                            <Text> 1 Nucamp Way</Text>
+                            <Text > Seattle, WA 98001</Text>
+                            <Text style={{marginBottom: 10}}> U.S.A. </Text>
+                            <Text> Phone: 1-206-555-1234</Text>
+                            <Text> campsites@nucamp.co</Text>
+                        </View>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         );
     }
