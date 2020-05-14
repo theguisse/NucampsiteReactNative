f7214fb74842347fd736e1d2233a686afb4f695b
Joe Peterson <josephbpeterson@gmail.com>
2020-05-13 21:52:38 -0700

    Social Sharing

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
 
@@ -19,14 +21,34 @@ class Reservation extends Component {
     static navigationOptions = {
         title: 'Reserve Campsite'
     }
-
-    toggleModal() {
-        this.setState({showModal: !this.state.showModal});
-    }    
-
+    
     handleReservation() {
         console.log(JSON.stringify(this.state));
-        this.toggleModal();
+        const message = `Number of Campers: ${this.state.campers}
+                        \nHike-In ${this.state.hikeIn}
+                        \nDate: ${this.state.date}`;
+        Alert.alert(
+            'Begin Search?',
+            message,
+            [
+                {
+                    text: 'Cancel',
+                    onPress: () => {
+                        console.log('Reservation Search Canceled');
+                        this.resetForm();
+                    },
+                    style: 'cancel'
+                },
+                {
+                    text: 'OK',
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date);
+                        this.resetForm();
+                    }
+                }
+            ],
+            {cancelable: false}
+        );
     }
 
     resetForm() {
@@ -36,6 +58,28 @@ class Reservation extends Component {
             date: '',
             showModal: false
         });
+    }
+
+    async obtainNotificationPermission() {
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission()
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + 'requested'
+            });
+        }
     }
 
     render() {
@@ -94,50 +138,13 @@ class Reservation extends Component {
                     />
                 </View>
                 <View style={styles.formRow}>
-                    <Button title= "search"
-                         onPress = { () => {
-                            Alert.alert(
-                                'Begin Search?',
-                                'Number of Campers: ' + this.state.campers +
-                                '\nHike In?' + this.state.hikeIn +
-                                '\nDate:' + this.state.date,
-                                [
-                                    {
-                                        text: 'Cancel',
-                                        style: 'cancel',
-                                        onPress :() => this.resetForm()
-                                    },
-                                    {
-                                        text: 'OK',
-                                        
-                                    }
-                                ],
-                                { cancelable: false }
-                            )
-                        }
-                    }
+                    <Button
+                        onPress={() => this.handleReservation()}
+                        title='Search'
+                        color='#5637DD'
+                        accessibilityLabel='Tap me to search for available campsites to reserve'
                     />
                 </View>
-                {/* <Modal
-                    animationType={'slide'}
-                    transparent={false}
-                    visible={this.state.showModal}
-                    onRequestClose={() => this.toggleModal()}>
-                    <View style={styles.modal}>
-                        <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-                        <Text style={styles.modalText}>Number of Campers: {this.state.campers}</Text> 
-                        <Text style={styles.modalText}>Hike-In?: {this.state.hikeIn ? 'Yes' : 'No'}</Text>
-                        <Text style={styles.modalText}>Date: {this.state.date}</Text>
-                        <Button
-                            onPress={() => {
-                                this.toggleModal();
-                                this.resetForm();
-                            }}
-                            color='#5637DD'
-                            title='Close'
-                        />
-                    </View>    
-                </Modal> */}
             </Animatable.View>
         );
     }
